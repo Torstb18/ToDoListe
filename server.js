7be48e7499c1a1180ac882947f8d58a430e8bd2e
Torstb18 <70886231+Torstb18@users.noreply.github.com>
2020-11-27 13:00:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const fs = require("fs");
-const express = require("express");
-const {client} = require("pg")
-const app = express();
-app.use("/JS", express.static('./JS/'));
-app.use(express.static('public'));
-app.use(express.json());
-
-
   
+const fs = require("fs");
+const bodyParser = require('body-parser');
+const {client} = require("pg")
+const express = require("express");
+const server= express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
-var port = process.env.PORT||3000;
-app.listen(port, function(){
-    console.log("server listening on port 3000!");
-});
+
+express()
+  .use ("/JS", express.static("./JS"))
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'server'))
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
